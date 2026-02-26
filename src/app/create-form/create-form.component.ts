a6faa426e7af66feeb1e038127c5f39264ef0fc1
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-02-26 08:30:00 +0100

    Update to v19 Angular/Cli

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-create-form',
-  templateUrl: './create-form.component.html',
-  styleUrls: ['./create-form.component.css']
+    selector: 'app-create-form',
+    templateUrl: './create-form.component.html',
+    styleUrls: ['./create-form.component.css'],
+    standalone: false
 })
 export class CreateFormComponent {
 
--- src/app/edit-form/edit-form.component.ts
+++ src/app/edit-form/edit-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-form',
-  templateUrl: './edit-form.component.html',
-  styleUrls: ['./edit-form.component.css']
+    selector: 'app-edit-form',
+    templateUrl: './edit-form.component.html',
+    styleUrls: ['./edit-form.component.css'],
+    standalone: false
 })
 export class EditFormComponent {
 
