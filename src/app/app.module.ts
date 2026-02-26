bfa82ee560e68c961790810f86130a7b104c50e3
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-02-26 08:30:00 +0100

    Modification de la page d'accueil de base. Ajout de nouvelles routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateFormComponent } from './create-form/create-form.component';
+import { EditFormComponent } from './edit-form/edit-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateFormComponent,
+    EditFormComponent
   ],
   imports: [
     BrowserModule,
