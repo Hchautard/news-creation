9f5b7d22c2fedac6927c96377ffd1b8dcb4cc472
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-03-03 22:16:50 +0100

    Enhanced edit form by adding bulma classes

--- src/app/components/edit-form/edit-form.component.ts
+++ src/app/components/edit-form/edit-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 
 @Component({
-    selector: 'app-edit-form',
-    templateUrl: './edit-form.component.html',
-    styleUrls: ['./edit-form.component.css'],
-    standalone: true
+  selector: 'app-edit-form',
+  templateUrl: './edit-form.component.html',
+  styleUrls: ['./edit-form.component.css'],
+  imports: [FormsModule, MatFormField, MatInput, MatLabel],
+
+  standalone: true
 })
 export class EditFormComponent {
+  formData = {
+    title: '',
+    description: '',
+    content: '',
+    date_event: '',
+    category: '',
+    location: '',
+    image: null,
+  };
 
+  updateNews() {
+    console.log('News item created:', this);
+  }
+
+  cancelEdit() {
+    console.log('Edit cancelled');
+  }
 }
